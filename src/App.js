e282aaccee037544a2f0a914ec004a721c52803b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-05-06 14:32:45 +0900

    クリックした曲を再生

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { SongList } from "./lib/components/SongList";
 import spotify from "./lib/spotify";
+import { useRef } from "react";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [popularSongs, setPopularSongs] = useState([]);
+  const [isPlay, setIsPlay] = useState(false);
+  const [selectedSong, setSelectedSong] = useState();
+  const audioRef = useRef(null);
 
   useEffect(() => {
     fetchPopularSongs();
@@ -19,6 +23,14 @@ export default function App() {
     setPopularSongs(popularSongs);
     setIsLoading(false);
   };
+
+  const handleSongSelected = async (song) => {
+    setSelectedSong(song);
+    audioRef.current.src = song.preview_url;
+    audioRef.current.play();
+    setIsPlay(true);
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
@@ -27,8 +39,13 @@ export default function App() {
         </header>
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
-          <SongList isLoading={isLoading} songs={popularSongs} />
+          <SongList
+            isLoading={isLoading}
+            songs={popularSongs}
+            onSongSelected={handleSongSelected}
+          />
         </section>
+        <audio ref={audioRef} />
       </main>
     </div>
   );
